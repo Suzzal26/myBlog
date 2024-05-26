e2ee7c9e3163d14c234146cad4387fb04828cc91
Suzzal26 <suzzalghimire@gmail.com>
2024-05-26 18:34:45 +0545

    added routing using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <>
-      <Login />
-      <Register />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 };
